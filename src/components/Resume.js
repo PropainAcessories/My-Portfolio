5f590e14525a2ae1cb21c93f50bbffcd0eae5224
HankHill <hhc3h8@gmail.com>
2022-11-28 14:14:34 -0600

    resume pdf

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,35 @@
 import React from "react";
+import '../App.css';
+import '../styles/Portfolio.css';
 
 function Resume() {
     return(
-        <div></div>
+        <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <h4>Download Resume
+
+                            </h4>
+                        </div>
+
+                        <div className="modal-footer"/>
+
+                        <div className="header">
+                            <h4>Skills</h4>
+                        </div>
+                        <div className="modal-footer"/>
+
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="">Technologies</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
     );
 }
 
