3433a8acf62739fc7a074077ba4052edcbf344d1
HankHill <hhc3h8@gmail.com>
2022-11-28 15:16:09 -0600

    Updates

---
+++ precache-manifest.8e2176f436e6ec60ddab3806b935da54.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a03b8abb68525d979eb",
+    "url": "/My-Portfolio/static/css/main.78ba2f10.chunk.css"
+  },
+  {
+    "revision": "4a03b8abb68525d979eb",
+    "url": "/My-Portfolio/static/js/main.4a03b8ab.chunk.js"
+  },
+  {
+    "revision": "093ecf0bec58628b4322",
+    "url": "/My-Portfolio/static/js/1.093ecf0b.chunk.js"
+  },
+  {
+    "revision": "d09a7290b088a11b217c",
+    "url": "/My-Portfolio/static/css/2.eb115f06.chunk.css"
+  },
+  {
+    "revision": "d09a7290b088a11b217c",
+    "url": "/My-Portfolio/static/js/2.d09a7290.chunk.js"
+  },
+  {
+    "revision": "97c6b6aeb27bc267dec0",
+    "url": "/My-Portfolio/static/js/runtime~main.97c6b6ae.js"
+  },
+  {
+    "revision": "2a4ff0a31c1b38d5530b7a11baeb7dd1",
+    "url": "/My-Portfolio/static/media/Me.2a4ff0a3.jpg"
+  },
+  {
+    "revision": "a9815532a68d04d38f5f0377d94dbf95",
+    "url": "/My-Portfolio/static/media/MusicPilot.a9815532.png"
+  },
+  {
+    "revision": "dc4e6d9959101dd66361d128324c874a",
+    "url": "/My-Portfolio/static/media/BankApp.dc4e6d99.png"
+  },
+  {
+    "revision": "979bd1031d386892ba96933ca5713426",
+    "url": "/My-Portfolio/static/media/NoteTaker.979bd103.png"
+  },
+  {
+    "revision": "09f04a14e67047bc95c244aa12140788",
+    "url": "/My-Portfolio/static/media/PwaTextEditor.09f04a14.png"
+  },
+  {
+    "revision": "840ccc5b505302ce577452999826c6c7",
+    "url": "/My-Portfolio/static/media/TechForum.840ccc5b.png"
+  },
+  {
+    "revision": "2d22ebec4a259a436f7e72dfc102ccd4",
+    "url": "/My-Portfolio/static/media/WeatherDashboard.2d22ebec.png"
+  },
+  {
+    "revision": "14e03cfe590e9dd25d9a6eed359cf430",
+    "url": "/My-Portfolio/index.html"
+  }
+];
